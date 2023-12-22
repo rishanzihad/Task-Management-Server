5047f07e7b13ead4912e4cf686e37135395dab11
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-12-22 17:34:42 +0600

    task drag and drop Done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express =require('express')
-const app =express()
-const cors =require("cors")
+const express = require('express')
+const app = express()
+const cors = require("cors")
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5009;
-
+const { ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
@@ -25,68 +25,91 @@ async function run() {
     //await client.connect();
     const userCollection = client.db("TaskDb").collection("user");
     const taskCollection = client.db("TaskDb").collection("task");
-     // jwt related api
-     app.post('/jwt', async (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        res.send({ token });
-      })
-  
-      // middlewares 
-      const verifyToken = (req, res, next) => {
-        console.log('inside verify token', req.headers.authorization);
-        if (!req.headers.authorization) {
-          return res.status(401).send({ message: 'unauthorized access' });
-        }
-        const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-          if (err) {
-            return res.status(401).send({ message: 'unauthorized access' })
-          }
-          req.decoded = decoded;
-          next();
-        })
-      }
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
 
-      app.post('/task',verifyToken, async (req, res) => {
-        const task = req.body;
-        const result = await taskCollection.insertOne(task);
-        res.send(result);
-      });
-      app.get('/task', async (req, res) => {
-        const result = await taskCollection.find().toArray();
-        res.send(result);
-      })
-     
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-      
-        const query = { email: user.email }
-        const existingUser = await userCollection.findOne(query);
-        if (existingUser) {
-          return res.send({ message: 'user already exists', insertedId: null })
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
         }
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-      });
-      app.get('/users',verifyToken,  async (req, res) => {
-        const result = await userCollection.find().toArray();
-        res.send(result);
+        req.decoded = decoded;
+        next();
       })
+    }
+
+    app.post('/task', verifyToken, async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+    app.get('/task', async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    })
+    // Update task order endpoint
+   // Example server code
+app.put('/task/status', async (req, res) => {
+  const { updatedTasks } = req.body;
+
+  try {
+      const bulkOps = updatedTasks.map(({ _id, status }) => ({
+          updateOne: {
+              filter: { _id: new ObjectId(_id) },
+              update: { $set: { status } },
+          },
+      }));
+
+      await taskCollection.bulkWrite(bulkOps);
+      res.send({ message: 'Task status updated successfully' });
+  } catch (error) {
+      console.error('Error updating task status:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+    
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get('/users', verifyToken, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-   // await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Task is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Task is Running ${port}`);
-  })
+  res.send('Task is running')
+})
+
+app.listen(port, () => {
+  console.log(`Task is Running ${port}`);
+})
